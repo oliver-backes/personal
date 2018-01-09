07e97f0a34bba58f32675fecb9d9e50f37d6852b
Oliver Backes <olb413@student.hks.harvard.edu>
2018-01-09 14:22:23 -0500

    add initial input tracking

--- personal_tracking.py
+++ personal_tracking.py
@@ -11,21 +11,41 @@ print "- exercise - minutes;"
 print "- water (fl oz);"
 print "- and OEV count."
 
+inputs = []
+
 date = input('Enter the date: ')
+
+inputs.append(date)
 
 weight = input('Enter your weight: ')
 
+inputs.append(weight)
+
 calories = input('Enter caloric intake: ')
+
+inputs.append(calories)
 
 steps = input('Enter step count: ')
 
+inputs.append(steps)
+
 exercisecount = input('Enter exercise count: ')
+
+inputs.append(exercisecount)
 
 exercisemin = input('Enter exercise minutes: ')
 
+inputs.append(exercisemin)
+
 water = input('Enter water intake: ')
 
+inputs.append(water)
+
 oev = input('Enter OEV count: ')
+
+inputs.append(oev)
+
+print inputs
 
 print "Here are the personal health indicators that you entered. Are they correct?"
 
